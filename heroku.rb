ca4dcb28f3eaaf27b283d4fa699efdc3d1357010
Matt Mondok <matt.mondok@gmail.com>
2013-05-28 17:51:17 -0400

    Cleanup

--- heroku.rb
+++ heroku.rb
@@ -5,22 +5,20 @@
 require "rubygems" # ruby1.9 doesn't "require" it though
 require "thor"
 
-class HerokuThor < Thor                                                 # [1]
-  package_name "Heroku"                                             # [2]
-  map "-L" => :list                                              # [3]
+class HerokuThor < Thor                                                
+  package_name "Heroku"                                             
+  map "-L" => :list                                              
   
-  desc "pull DATABASE_NAME", "pull the latest database from heroku"   # [4]
+  desc "pull DATABASE_NAME", "pull the latest database from heroku"   
   def pull_and_restore(database_name)
-    %x(heroku pgbackups:capture && curl -o latest.dump `heroku pgbackups:url`)
-    db_str = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d #{database_name} latest.dump"
-    exec db_str
-    %x(rm latest.dump)
+    exec "heroku pgbackups:capture && curl -o latest.dump `heroku pgbackups:url`"
+    exec "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d #{database_name} latest.dump"
+    exec "rm latest.dump"
   end
 
-  desc "pull DATABASE_NAME", "pull the latest database from heroku"   # [4]
+  desc "push DATABASE_NAME", "pull the latest database to dropbox"  
   def push(database_name)
-    db_str = "pg_dump -Fc --no-acl --no-owner -h localhost -U postgres #{database_name} > ~/Dropbox/Public/#{database_name}.dump"
-    exec db_str
+    exec "pg_dump -Fc --no-acl --no-owner -h localhost -U postgres #{database_name} > ~/Dropbox/Public/#{database_name}.dump"
   end  
 end
 
