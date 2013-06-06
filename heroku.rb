d095db9bf0daa6bc8d249abf0ffadd3bbd48470f
Matt Mondok <matt.mondok@gmail.com>
2013-06-06 13:14:26 -0400

    Add autodelete of oldest backup

--- heroku.rb
+++ heroku.rb
@@ -11,7 +11,7 @@ class HerokuThor < Thor
   
   desc "pull_and_restore DATABASE_NAME", "pull the latest database from heroku"   
   def pull_and_restore(database_name)
-    exec "heroku pgbackups:capture && curl -o latest.dump `heroku pgbackups:url` && pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d #{database_name} latest.dump && rm latest.dump"
+    exec "heroku pgbackups:capture --expire && curl -o latest.dump `heroku pgbackups:url` && pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d #{database_name} latest.dump && rm latest.dump"
   end
 
   desc "push DATABASE_NAME", "pull the latest database to dropbox"  
